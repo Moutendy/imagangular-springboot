d5ecda4b4421af3703851227f27171907e6dd281
c.moutendy <c.moutendy@isicod.net>
2022-08-01 16:27:19 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private httpClient: HttpClient) { }
   title = 'ImageUpload';
+  selectedFile!: File;
+retrievedImage: any;
+base64Data: any;
+retrieveResonse: any;
+message?: string;
+imageName: any;
+ngOnInit()
+{
+this.getImage();
+}
+
+public onFileChanged(event: any) {
+  //Select File
+  this.selectedFile = event.target.files[0];
+}
+
+
+
+onUpload() {
+  console.log(this.selectedFile);
+  
+  //FormData API provides methods and properties to allow us easily prepare form data to be sent with POST HTTP requests.
+  const uploadImageData = new FormData();
+  uploadImageData.append('imageFile', this.selectedFile, this.selectedFile.name);
+
+    //Make a call to the Spring Boot Application to save the image
+    this.httpClient.post('http://localhost:8083/image/upload', uploadImageData, { observe: 'response' })
+      .subscribe((response) => {
+        if (response.status === 200) {
+          this.message = 'Image uploaded successfully';
+        } else {
+          this.message = 'Image not uploaded successfully';
+        }
+      }
+      );
+}
+
+
+getImage() {
+  //Make a call to Sprinf Boot to get the Image Bytes.
+  this.httpClient.get('http://localhost:8083/image/get/' + this.imageName)
+    .subscribe(
+      res => {
+        this.retrieveResonse = res;
+        this.base64Data = this.retrieveResonse.picByte;
+        this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
+      }
+    );
+}
+  
 }
